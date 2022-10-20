512cc35d2e20fcda98aeac274e0e53b1d955f2f0
Skeevert <skeevert@Serenity>
2022-10-20 20:16:51 +0300

    Block direction fix, block name

--- src/main/java/net/fabricmc/traderblockmod/TraderBlock.java
+++ src/main/java/net/fabricmc/traderblockmod/TraderBlock.java
@@ -2,20 +2,33 @@ package net.fabricmc.traderblockmod;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockEntityProvider;
+import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.HorizontalFacingBlock;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.DirectionProperty;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.BlockMirror;
+import net.minecraft.util.BlockRotation;
 import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+
 
 import net.minecraft.world.World;
 
 public class TraderBlock extends Block implements BlockEntityProvider {
+    public static final DirectionProperty FACING = HorizontalFacingBlock.FACING;
+
 	public TraderBlock(Settings settings) {
 		super(settings);
+		this.setDefaultState((BlockState)((BlockState)((BlockState)this.stateManager.getDefaultState()).with(FACING, Direction.NORTH)));
+
 	}
 	
 	@Override
@@ -37,7 +50,33 @@ public class TraderBlock extends Block implements BlockEntityProvider {
 	}
 
 	@Override
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        return (BlockState)this.getDefaultState().with(FACING, ctx.getPlayerFacing().getOpposite());
+    }
+
+	@Override
 	public BlockEntity createBlockEntity(BlockPos arg0, BlockState arg1) {
 		return new TraderBlockEntity(arg0, arg1);
 	}
+	
+	@Override
+	protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+		builder.add(FACING);
+	}
+	
+    @Override
+    public BlockRenderType getRenderType(BlockState state) {
+        return BlockRenderType.MODEL;
+    }
+
+    @Override
+    public BlockState rotate(BlockState state, BlockRotation rotation) {
+        return (BlockState)state.with(FACING, rotation.rotate(state.get(FACING)));
+    }
+
+    @Override
+    public BlockState mirror(BlockState state, BlockMirror mirror) {
+        return state.rotate(mirror.getRotation(state.get(FACING)));
+    }
+
 }
